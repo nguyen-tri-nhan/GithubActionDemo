ffd5d88e7bb42dc2d7327b49e828a90cb50471fb
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2021-01-20 16:54:20 +0700

    init

---
+++ src/test/java/MathUtilTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MathUtilTest {
+    MathUtil util = new MathUtil();
+
+    @Test
+    void test1(){
+        assertEquals(util.sum(1,1),2);
+    }
+//    @Test
+//    void test2(){
+//        assertEquals(util.sum(1,1),3);
+//    }
+    @Test
+    void test3(){
+        assertEquals(util.sum(1,2),3);
+    }
+}
